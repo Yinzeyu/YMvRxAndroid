4febe86868acac79693878953c74e7b42196113d
yinzeyu <13641153277@163.com>
2019-11-06 15:42:50 +0800

    更新现有模块下所有东西

--- app/src/main/java/com/yzy/example/component/user/HomeActivity.kt
+++ app/src/main/java/com/yzy/example/component/user/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.yzy.example.component.user
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.yzy.example.R
 
 @Route(path = "/test/homeActivity")
 class HomeActivity : AppCompatActivity() {
