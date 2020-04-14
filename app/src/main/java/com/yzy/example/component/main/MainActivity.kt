00e57b2ae9daec9f0d199db690dbf568d524bcd0
yzy <136411532772163.com>
2020-04-14 20:06:12 +0800

    修改baseFragment 跟布局 未使用layout导致现实问题

--- app/src/main/java/com/yzy/example/component/main/MainActivity.kt
+++ app/src/main/java/com/yzy/example/component/main/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.yzy.example.component.main
 
 import android.content.Context
-import android.view.MotionEvent
 import com.blankj.utilcode.util.ActivityUtils
 import com.yzy.baselibrary.base.BaseActivity
 import com.yzy.baselibrary.extention.startActivity
 import com.yzy.baselibrary.extention.toast
-import com.yzy.baselibrary.utils.MyTouchListener
 import com.yzy.example.R
 
 
@@ -17,25 +15,13 @@ class MainActivity : BaseActivity() {
             context.startActivity<MainActivity>()
         }
     }
-
-
-
-    //不设置状态栏填充，即显示全屏
     override fun layoutResId(): Int = R.layout.activity_main
 
     override fun initView() {
-//        val navController = Navigation.findNavController(this, R.id.nav_fragment)
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment?
-//        val navigator = KeepStateNavigator(this, navHostFragment!!.childFragmentManager, R.id.nav_fragment)
-//        navController.navigatorProvider.addNavigator(navigator)
-//        navController.setGraph(R.navigation.wan_navigation)
     }
-
     override fun initData() {
-        //关闭其他所有页面
         ActivityUtils.finishOtherActivities(javaClass)
     }
-
     private var touchTime = 0L
     private val waitTime = 2000L
     override fun onBackPressed() {
