87c27ea833bc4680b0596e2afd09fad688146000
宇哥 <13641153277>
2020-04-17 17:29:42 +0800

    修改个版本

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,16 +6,11 @@ object Versions {
     const val support = "1.0.0"
     const val constraint = "1.1.3"
     const val lifecycle = "2.0.0"
-    const val junit = "4.12"
-    const val runner = "1.1.0"
-    const val espresso = "3.1.0"
-    const val androidGradlePlugin = "3.6.2"
+    const val androidGradlePlugin = "4.0.0-beta04"
     const val navigationPlugin = "2.2.2"
     const val kotlin = "1.3.72"
     const val retrofit = "2.3.0"
     const val okHttpLoggingInterceptor = "3.4.1"
-    const val rxKotlin2 = "2.2.0"
-    const val rxAndroid = "2.0.2"
     const val glide = "4.8.0"
 }
 
