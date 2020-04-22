d64617327e7e49228b1d210cc62e80f6ccd9be2e
yzy <136411532772163.com>
2020-04-22 21:53:40 +0800

    修改请求头添加问题

--- app/src/main/java/com/yzy/example/http/RequestIntercept.kt
+++ app/src/main/java/com/yzy/example/http/RequestIntercept.kt
@@ -1,18 +1,11 @@
 package com.yzy.example.http
 
-import android.text.TextUtils
 import android.util.Log
-import com.google.gson.Gson
-import com.yzy.example.constants.ApiConstants
 import com.yzy.example.http.integration.HeaderManger
-import okhttp3.FormBody
 import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.Interceptor
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
-import okio.Buffer
 import java.io.IOException
 
 /**
@@ -20,6 +13,52 @@ import java.io.IOException
  *@date 2019/7/15
  *@author: yzy.
  */
+class RequestIntercept : Interceptor {
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        var builder = request.newBuilder()
+        var newBaseUrl: HttpUrl? = null
+        val dynamicBaseUrl = HeaderManger.getInstance().getDynamicBaseUrl()
+        val headerValues = request.headers("urlName")
+        if ((null !=dynamicBaseUrl  && "" != dynamicBaseUrl)|| headerValues.isNotEmpty()) {
+            if (headerValues.isNotEmpty()) {
+                //如果有这个header，先将配置的header删除，因此header仅用作app和okhttp之间使用
+                builder.removeHeader("urlName")
+                //匹配获得新的BaseUrl
+                val headerValue = headerValues[0]
+                val headerUrlValue = "Domain"
+                if (headerValue.contains(headerUrlValue)) {
+                    val split = headerValue.substring(headerUrlValue.length)
+                    if (split.isNotEmpty()){
+                        newBaseUrl = split.toHttpUrlOrNull()
+                    }
+                }
+            } else {
+                newBaseUrl = dynamicBaseUrl?.toHttpUrlOrNull()
+                HeaderManger.getInstance().setDynamicBaseUrl(null)
+            }
+            //重建新的HttpUrl，修改需要修改的url部分
+            val newFullUrl = request.url
+                .newBuilder()
+                .scheme("https")//更换网络协议
+                .host(newBaseUrl?.host ?: request.url.host)//更换主机名
+                .port(newBaseUrl?.port ?: request.url.port)//更换端口
+                .build()
+            Log.e("Url", "intercept: $newFullUrl")
+            builder = builder.url(newFullUrl)
+        }
+        return chain.proceed(builder.build())
+
+    }
+
+}
+
+
+
+
+
 
 //class RequestIntercept : Interceptor {
 //
@@ -167,44 +206,4 @@ import java.io.IOException
 //
 //}
 
-class RequestIntercept : Interceptor {
 
-    @Throws(IOException::class)
-    override fun intercept(chain: Interceptor.Chain): Response {
-        var request = chain.request()
-        val builder = request.newBuilder()
-        var newBaseUrl: HttpUrl? = null
-        val dynamicBaseUrl = HeaderManger.getInstance().getDynamicBaseUrl()
-        val headerValues = request.headers("urlName")
-        if ((null !=dynamicBaseUrl  && "" != dynamicBaseUrl)|| headerValues.isNotEmpty()) {
-            if (headerValues.isNotEmpty()) {
-                //如果有这个header，先将配置的header删除，因此header仅用作app和okhttp之间使用
-                builder.removeHeader("urlName")
-                //匹配获得新的BaseUrl
-                val headerValue = headerValues[0]
-                val headerUrlValue = "Domain"
-                if (headerValue.contains(headerUrlValue)) {
-                    val split = headerValue.substring(headerUrlValue.length)
-                    if (split.isNotEmpty()){
-                        newBaseUrl = split.toHttpUrlOrNull()
-                    }
-                }
-            } else {
-                newBaseUrl = dynamicBaseUrl?.toHttpUrlOrNull()
-                HeaderManger.getInstance().setDynamicBaseUrl(null)
-            }
-            //重建新的HttpUrl，修改需要修改的url部分
-            val newFullUrl = request.url
-                .newBuilder()
-                .scheme("https")//更换网络协议
-                .host(newBaseUrl?.host ?: request.url.host)//更换主机名
-                .port(newBaseUrl?.port ?: request.url.port)//更换端口
-                .build()
-            Log.e("Url", "intercept: $newFullUrl")
-            request = builder.url(newFullUrl).build()
-        }
-        return chain.proceed(request)
-
-    }
-
-}
